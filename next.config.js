185152fe5154431cecc4fe325b6e3a1a13fb3bf1
ahmadysalah <a.ahmad.salah@gmail.com>
2023-04-24 01:16:27 +0200

    upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_MONGODB_URI: 'mongodb://localhost:27019/nextjs'
+  },
 }
 
 module.exports = nextConfig
