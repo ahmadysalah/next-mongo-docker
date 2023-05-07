f83c0532564108b7148e73c34db0c245a840e132
ahmadysalah <a.ahmad.salah@gmail.com>
2023-05-07 12:58:08 +0300

    update db

--- pages/api/authors.js
+++ pages/api/authors.js
@@ -2,9 +2,8 @@ import Blogs from '../../lib/controllers/blogs'
 import { errorResponse, successResponse } from '../../lib/helper'
 
 export default async function handler(req, res) {
-  console.log('id', req)
   try {
-    const id = req?.query?.id || req?.params?.id
+    const id = req?.query?.id
     switch (req.method) {
       case 'GET':
         const blogs = await Blogs.getBlogs(id)
@@ -13,21 +12,12 @@ export default async function handler(req, res) {
       case 'POST':
         const newBlog = await Blogs.createBlog(req.body)
         successResponse(res, { data: newBlog })
-        break
-      case 'PATCH':
-        const updatedBlog = await Blogs.updateBlogById(id, req.body)
-        successResponse(res, { data: updatedBlog })
-        break
-      case 'DELETE':
-        const deletedBlog = await Blogs.deleteBlogById(id)
-        successResponse(res, { data: deletedBlog })
-        break
       default:
         errorResponse(res, { message: 'Bad request method' })
         break
     }
   } catch (error) {
-    errorResponse(res, { error, message: error.message || 'Internal server error' })
+    errorResponse(res, { message: error.message || 'Internal server error' })
   }
 }
 
